ecf16614763dd85ca37ba4c921307f345ecb8ea7
Jeff2Ma <mjh2316@gmail.com>
2017-04-17 23:40:06 +0800

    Add others.

---
+++ README.md
@@ -0,0 +1,62 @@
+# gulp-qcloud-upload
+
+> Qcloud COS upload plugin for gulpjs. 腾讯云COS 上传插件
+> 
+> 适合于腾讯云最新的[COSV4](https://console.qcloud.com/cos4)（云对象存储v4）版本，基于官方Node.js SDK 进行开发。
+
+## 安装
+
+```bash
+npm i gulp-qcloud-upload -S
+```
+
+## 配置
+
+```javascript
+var gulp = require('gulp');
+var qcloudUpload = require('./index.js');
+
+gulp.task('default', function () {
+    return gulp.src('./test/**/*')
+        .pipe(qcloudUpload({
+            appid: 'youor appid',
+            secretId: 'your secretId',
+            secretKey: 'your secretKey',
+            bucket: 'bucket-name',
+            region: 'cn-east',
+            prefix: 'my-diy-prefix',
+            overWrite: false
+        }));
+});
+```
+## 效果
+
+![效果](./test/example.png)
+
+## 选项
+
+### appid、secretId、secretKey
+
+以上前往 [https://console.qcloud.com/capi](https://console.qcloud.com/capi) 获取APPID 与密钥信息。
+
+### bucket
+
+Bucket 名称。
+
+### region
+
+Bucket 所属地区。国内目前有华南、华东、华北区可选，对应值为：`cn-south`,`cn-east`,`cn-north`
+
+### prefix
+
+自定义COS 中的文件前缀，输入的字符串不包括文件名部分。[详情](https://www.qcloud.com/document/product/436/6237 )
+
+### overWrite
+
+若COS 中已经有同名文件，是否覆盖。默认`false`。
+
+## 链接
+
+本插件基于[gulp-qcloud](https://www.npmjs.com/package/gulp-qcloud) 改进，修复其在COSV4 上不能使用的问题。
+
+腾讯云官方文档：[https://www.qcloud.com/document/product/436/8629](https://www.qcloud.com/document/product/436/8629)
--- index.js
+++ index.js
@@ -9,91 +9,91 @@ var merge = require('lodash.merge');
 var cos = require('./sdk/cos');
 
 module.exports = function (option) {
-    option = option || {};
-    option = merge({
-        appid: option.appid || '',
-        secretId: option.secretId || '',
-        secretKey: option.secretKey || '',
-        bucket: option.bucket || '', // Bucket 名称
-        region: option.region || '', // 地区，cn-south,cn-east,cn-north
-        prefix: option.prefix || '', // 自定义文件前缀
-        overWrite: option.overWrite || false // 是否覆盖文件
-    }, option);
+	option = option || {};
+	option = merge({
+		appid: option.appid || '',
+		secretId: option.secretId || '',
+		secretKey: option.secretKey || '',
+		bucket: option.bucket || '', // Bucket 名称
+		region: option.region || '', // 地区，cn-south,cn-east,cn-north
+		prefix: option.prefix || '', // 自定义文件前缀
+		overWrite: option.overWrite || false // 是否覆盖文件
+	}, option);
 
-    var nowTime = Moment().format('YYMMDDHHmm');
-    var existFiles = 0;
-    var uploadedFiles = 0;
-    var uploadedFail = 0;
-    var tasks = [];
+	var nowTime = Moment().format('YYMMDDHHmm');
+	var existFiles = 0;
+	var uploadedFiles = 0;
+	var uploadedFail = 0;
+	var tasks = [];
 
-    return through2.obj(function (file, enc, next) {
-        var that = this;
-        if (file._contents === null) return next();
+	return through2.obj(function (file, enc, next) {
+		var that = this;
+		if (file._contents === null) return next();
 
-        // 相对路径
-        var fileRelative = path.relative(file.base, file.path);
+		// 相对路径
+		var fileRelative = path.relative(file.base, file.path);
 
-        // 保存在bucket 的前缀，模拟子目录的概念
-        var filePrefix = option.prefix;
+		// 保存在bucket 的前缀，模拟子目录的概念
+		var filePrefix = option.prefix;
 
-        // 待上传文件的本地绝对路径
-        var filePath = file.path;
+		// 待上传文件的本地绝对路径
+		var filePath = file.path;
 
-        // 保存在bucket 的file key
-        var fileKey = path.join(filePrefix, fileRelative);
+		// 保存在bucket 的file key
+		var fileKey = path.join(filePrefix, fileRelative);
 
-        // 获取到文件字节大小
-        var stats = fs.statSync(filePath);
-        var fileSizeInBytes = stats.size;
+		// 获取到文件字节大小
+		var stats = fs.statSync(filePath);
+		var fileSizeInBytes = stats.size;
 
-        var handler = function () {
-            var defer = Q.defer();
+		var handler = function () {
+			var defer = Q.defer();
 
-            var paramsPutObj = {
-                Appid: option.appid,
-                SecretId: option.secretId,
-                SecretKey: option.secretKey,
-                Bucket: option.bucket,
-                Region: option.region,
-                IfModifiedSince: nowTime,
-                Key: fileKey,
-                Body: filePath,
-                ContentLength: fileSizeInBytes
-            };
+			var paramsPutObj = {
+				Appid: option.appid,
+				SecretId: option.secretId,
+				SecretKey: option.secretKey,
+				Bucket: option.bucket,
+				Region: option.region,
+				IfModifiedSince: nowTime,
+				Key: fileKey,
+				Body: filePath,
+				ContentLength: fileSizeInBytes
+			};
 
-            cos.headObject(paramsPutObj, function (err, data) {
-                // 已经存在的文件
-                if (data && !option.overWrite) {
-                    existFiles++;
-                    return defer.resolve();
-                }
-                uploadedFiles++;
+			cos.headObject(paramsPutObj, function (err, data) {
+				// 已经存在的文件
+				if (data && !option.overWrite) {
+					existFiles++;
+					return defer.resolve();
+				}
+				uploadedFiles++;
 
-                cos.putObject(paramsPutObj, function (err, data) {
-                    log('Uploading:', colors.green(fileRelative), '→', colors.yellow(fileKey));
-                    // 上传失败
-                    if (err) {
-                        log('Error →', colors.red(fileKey), err.error.Message);
-                        uploadedFail++;
-                        defer.reject();
-                    }
-                    // 上传成功
-                    defer.resolve();
-                });
-            });
-            return defer.promise;
-        };
-        tasks.push(handler());
-        next();
-    }, function () {
-        Q.allSettled(tasks)
-            .then(function (fulfilled) {
-                log('Total:', colors.green(fulfilled.length),
-                    'Skip:', colors.gray(existFiles),
-                    'Success:', colors.green(uploadedFiles - uploadedFail),
-                    'Failed:', colors.red(uploadedFail));
-            }, function (err) {
-                log('Failed upload files:', err.message);
-            });
-    });
+				cos.putObject(paramsPutObj, function (err, data) {
+					log('Uploading:', colors.green(fileRelative), '→', colors.yellow(fileKey));
+					// 上传失败
+					if (err) {
+						log('Error →', colors.red(fileKey), err.error.Message);
+						uploadedFail++;
+						defer.reject();
+					}
+					// 上传成功
+					defer.resolve();
+				});
+			});
+			return defer.promise;
+		};
+		tasks.push(handler());
+		next();
+	}, function () {
+		Q.allSettled(tasks)
+			.then(function (fulfilled) {
+				log('Total:', colors.green(fulfilled.length),
+					'Skip:', colors.gray(existFiles),
+					'Success:', colors.green(uploadedFiles - uploadedFail),
+					'Failed:', colors.red(uploadedFail));
+			}, function (err) {
+				log('Failed upload files:', err.message);
+			});
+	});
 };
