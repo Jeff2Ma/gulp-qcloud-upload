0e0f1e28b03af2fdf463a77745460b6772fd8e1d
Jeff2Ma <mjh2316@gmail.com>
2017-05-04 19:08:41 +0800

    Files change.

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var path = require('path');
 var colors = require('gulp-util').colors;
 var log = require('gulp-util').log;
 var through2 = require('through2');
-var Moment = require('moment');
 var Q = require('q');
 var merge = require('lodash.merge');
-var cos = require('./sdk/cos');
+
+// 待加入
+var qos = require('./qos-node-client/dist/index').default;
 
 module.exports = function (option) {
 	option = option || {};
@@ -15,16 +16,37 @@ module.exports = function (option) {
 		secretId: option.secretId || '',
 		secretKey: option.secretKey || '',
 		bucket: option.bucket || '', // Bucket 名称
-		region: option.region || '', // 地区，cn-south,cn-east,cn-north
+		region: option.region || '', // 地区，sh,gz,tj
 		prefix: option.prefix || '', // 自定义文件前缀
-		overWrite: option.overWrite || false // 是否覆盖文件
+		overWrite: option.overWrite || false, // 是否覆盖文件
+		headers: option.headers || false // 自定义header
 	}, option);
 
-	var nowTime = Moment().format('YYMMDDHHmm');
 	var existFiles = 0;
 	var uploadedFiles = 0;
 	var uploadedFail = 0;
 	var tasks = [];
+
+	// 创建一个client
+	var client = qos.createClient({
+		appId: option.appid,
+		secretId: option.secretId,
+		secretKey: option.secretKey,
+		region: option.region,
+		bucket: option.bucket
+	});
+
+	// 设置header
+	function setHeader(fileKey) {
+		var authority = client.sign({bucket: option.bucket, fileId: fileKey});
+		client.updateStat({bucket: option.bucket, fileId: fileKey, authority: authority, headers: option.headers})
+			.then(function (res) {
+				// console.log('stat: ', res.message);
+			})
+			.catch(function (err) {
+				// console.error('err: ', err);
+			});
+	}
 
 	return through2.obj(function (file, enc, next) {
 		var that = this;
@@ -42,45 +64,42 @@ module.exports = function (option) {
 		// 保存在bucket 的file key
 		var fileKey = path.join(filePrefix, fileRelative);
 
-		// 获取到文件字节大小
-		var stats = fs.statSync(filePath);
-		var fileSizeInBytes = stats.size;
+		var fileID = '/' + fileKey;
 
 		var handler = function () {
 			var defer = Q.defer();
+			var statCode;
 
-			var paramsPutObj = {
-				Appid: option.appid,
-				SecretId: option.secretId,
-				SecretKey: option.secretKey,
-				Bucket: option.bucket,
-				Region: option.region,
-				IfModifiedSince: nowTime,
-				Key: fileKey,
-				Body: filePath,
-				ContentLength: fileSizeInBytes
-			};
-
-			cos.headObject(paramsPutObj, function (err, data) {
-				// 已经存在的文件
-				if (data && !option.overWrite) {
-					existFiles++;
-					return defer.resolve();
-				}
-				uploadedFiles++;
-
-				cos.putObject(paramsPutObj, function (err, data) {
-					log('Uploading:', colors.green(fileRelative), '→', colors.yellow(fileKey));
-					// 上传失败
-					if (err) {
+			// 检测是否已经存在文件
+			client.stat({bucket: option.bucket, fileId: fileID})
+				.then(function (res) {
+					statCode = res.code;
+					return statCode;
+				})
+				.catch(function (err) {
+					statCode = err.statusCode;
+					return statCode;
+				})
+				.then(function (statCode) {
+					// statCode 为0 表示已经有同名文件上传上去了
+					if (statCode === 0 && !option.overWrite) {
+						existFiles++;
+						return defer.resolve();
+					}
+					// 如果文件不存在，则上传
+					uploadedFiles++;
+					client.upload({localFile: filePath, fileId: fileID}).then(function (res) {
+						// console.log('上传成功', res);
+						log('Uploading:', colors.green(fileRelative), '→', colors.yellow(fileKey));
+						// 上传成功
+						setHeader(fileID);
+						defer.resolve();
+					}).catch(function (err) {
 						log('Error →', colors.red(fileKey), err.error.Message);
 						uploadedFail++;
 						defer.reject();
-					}
-					// 上传成功
-					defer.resolve();
+					});
 				});
-			});
 			return defer.promise;
 		};
 		tasks.push(handler());
